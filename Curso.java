cb362ab4715ce7097d7f24cf55850467224beefd
DevCarol <149845712+carolinadalmeida@users.noreply.github.com>
2024-04-08 16:17:50 -0300

    Add files via upload

---
+++ Curso.java
@@ -0,0 +1,36 @@
+package POO;
+
+public class Curso {
+
+    private String nome;
+    private String disciplinas;
+    private String codigo;
+
+    public static boolean adicionarDisciplina (){
+        return false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(String disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
---
+++ Laboratorio.java
@@ -0,0 +1,36 @@
+package POO;
+
+public class Laboratorio {
+
+    String nome;
+    String localizacao;
+    String capacidade;
+
+    public static boolean  entrarLaboratorio(){
+        return false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public String getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(String capacidade) {
+        this.capacidade = capacidade;
+    }
+}
